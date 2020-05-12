a0a7289db9082c7ddc79b93d826f2889ad99c169
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-05-12 13:45:16 +0200

    Finish 6.10

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -4,7 +4,7 @@ import { voteForAnecdote } from '../reducers/anecdoteReducer'
 
 const Anecdotes = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
 
   const vote = (id) => {
     dispatch(voteForAnecdote(id))
---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,7 @@
+const initialState = 'This is the initial state of the notification'
+
+const reducer = (state = initialState, action) => {
+  return state
+}
+
+export default reducer
